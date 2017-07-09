1fc1bcfb9afe5d6c2eeff7c0ebeb4c7c9df89301
Acharya <abhiacharya3300@gmail.com>
2017-07-09 14:07:46 +0530

    Updated With PEP8 Indentation

--- portscanner/scanner.py
+++ portscanner/scanner.py
@@ -1,51 +1,67 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
 import json
 import socket
 import threading
 from Queue import Queue
+
 print_lock = threading.Lock()
-target = "127.0.0.1"
+target = '127.0.0.1'
 with open('blacklist.json') as x:
     blacklist = json.load(x)
 dictionary = {}
 list = []
 portnew = []
 portold = []
-def new():
+
+
+def new ():
     with open('output.json') as x:
         data = json.load(x)
-    for i in (data):
-        if "ports" in data[i]:
-            portold.append(data[i]["ports"])
+    for i in data:
+        if 'ports' in data[i]:
+            portold.append(data[i]['ports'])
+
+
 new()
+
+
 def portscanudp (port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         con = s.connect((target, port))
-        x = str(port) + ",," + str(socket.getservbyport(port, 'udp')) + ",," + 'udp'
+        x = str(port) + ',,' + str(socket.getservbyport(port, 'udp')) \
+            + ',,' + 'udp'
         list.append(x)
         con.close()
     except:
         pass
+
+
 def portscantcp (port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         con = s.connect((target, port))
         try:
-            x = str(port) + ",," + str(socket.getservbyport(port, 'tcp')) + ",," + 'tcp'
+            x = str(port) + ',,' + str(socket.getservbyport(port, 'tcp'
+                                                            )) + ',,' + 'tcp'
             list.append(x)
         except:
-            x = str(port) + ",," + "SERVICE_NOT_FOUND" + ",," + 'tcp'
+            x = str(port) + ',,' + 'SERVICE_NOT_FOUND' + ',,' + 'tcp'
             list.append(x)
         con.close()
     except:
         pass
+
+
 def threader ():
     while True:
         worker = q.get()
         portscanudp(worker)
         portscantcp(worker)
         q.task_done()
+
+
 q = Queue()
 for x in range(40):
     t = threading.Thread(target=threader)
@@ -64,7 +80,7 @@ for i in range(len(list)):
                 dictionary[i + 1]['ports'] = str(small_list[0])
                 dictionary[i + 1]['type of port'] = str(small_list[2])
                 dictionary[i + 1]['service'] = str(small_list[1])
-                dictionary[i + 1]['remarks'] =  blacklist[list[i]]
+                dictionary[i + 1]['remarks'] = blacklist[list[i]]
             except:
                 pass
     except:
@@ -72,6 +88,14 @@ for i in range(len(list)):
         dictionary[i + 1]['remarks'] = 'not in blacklisted ports'
         dictionary[i + 1]['service'] = str(small_list[1])
         dictionary[i + 1]['type of port'] = str(small_list[2])
+
+for i in set(portnew) - set(portold):
+    dictionary['NewlyOpen'] = {}
+    dictionary['NewlyOpen'][i] = 'NEW'
+with open('output.json', 'w') as x:
+    json.dump(dictionary, x, indent=8)
+
+
 for i in (set(portnew)-set(portold)):
     dictionary['NewlyOpen'] ={}
     dictionary['NewlyOpen'][i] ='NEW'
